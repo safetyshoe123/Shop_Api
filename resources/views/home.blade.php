e42b10d4ebd84497136e1e6428ee532114ee143c
SmartYeti <113034221+SmartYeti@users.noreply.github.com>
2023-12-21 09:31:15 +0800

    home fixed (#12)
    * walara gud
    
    * Added Views using TailwindCSS
    
    * shop dash created
    
    * register branch added
    
    * home fix
    
    ---------
    
    Co-authored-by: OJT User 3 <ojt3@DBIC.local>

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,15 +2,14 @@
 @extends('layout.app')
 @section('content')
   
-    <!-- <div class="flex p-20 justify-center"> -->
     <!-- component -->
     <div class="flex flex-col m-auto p-auto">
-        <h1 class="flex py-5 lg:px-20 md:px-10 px-5 lg:mx-40 md:mx-20 mx-5 font-bold text-4xl text-gray-800">
+        <h1 class="flex py-10 lg:px-40 md:px-10 px-5 lg:mx-50 md:mx-20 mx-5 font-bold text-4xl">
             Shop Dashboard
         </h1>
         <div class="flex overflow-x-scroll pb-10 hide-scroll-bar ">
 
-                <div class="card w-96 glass hover:shadow-xl overflow-hidden">
+                <!-- <div class="card w-96 glass hover:shadow-xl overflow-hidden">
                     <figure><img
                             src="https://static.vecteezy.com/system/resources/previews/000/616/494/original/home-logo-and-symbols-vector.jpg"
                             alt="car!" /></figure>
@@ -21,7 +20,22 @@
                             <button class="btn btn-primary">Details</button>
                         </div>
                     </div>
+                </div> -->
+                <div class="flex flex-nowrap lg:ml-10 md:ml-20 ml-10 ">
+                    <div class="card w-96 glass hover:shadow-xl overflow-hidden">
+                        <figure><img
+                                src="https://static.vecteezy.com/system/resources/previews/000/616/494/original/home-logo-and-symbols-vector.jpg"
+                                alt="car!" /></figure>
+                        <div class="card-body">
+                            <h2 class="card-title">Branch 1</h2>
+                            <p>Address</p>
+                            <div class="card-actions justify-end">
+                                <button class="btn btn-primary">Details</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
                 <div class="flex flex-nowrap lg:ml-10 md:ml-20 ml-10 ">
                     <div class="card w-96 glass hover:shadow-xl overflow-hidden">
                         <figure><img
@@ -50,8 +64,7 @@
                             </div>
                         </div>
                     </div>
-                </div>
-
+                 </div>
                 <div class="flex flex-nowrap lg:ml-10 md:ml-20 ml-10 ">
                     <div class="card w-96 glass hover:shadow-xl overflow-hidden">
                         <figure><img
@@ -72,11 +85,15 @@
                                 src="https://static.vecteezy.com/system/resources/previews/000/616/494/original/home-logo-and-symbols-vector.jpg"
                                 alt="car!" /></figure>
                         <div class="card-body">
-
                             <h2 class="card-title">Branch 5</h2>
-
-                <!-- </div> -->
-            </div>
+                            <p>Address</p>
+                            <div class="card-actions justify-end">
+                                <button class="btn btn-primary">Details</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
         </div>
-
+    </div>
     @endsection
--- resources/views/regshop.blade.php
+++ resources/views/regshop.blade.php
@@ -1,18 +1,19 @@
 @extends('layout.app')
 @section('content')
     <!-- component -->
-    <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
+    <!-- <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
     <link rel="stylesheet"
-        href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
+        href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css"> -->
 
-
+    <div class="hero min-h-screen  bg-base-200">
+    <div class="hero-content flex-col lg:flex-row align-items-center ">
     <section class=" py-1 bg-blueGray-50">
         <div class="w-full lg:w-8/12 px-4 mx-auto mt-6">
             <div
-                class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
-                <div class="rounded-t bg-white mb-0 px-6 py-6">
+                class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg border-0">
+                <div class="rounded-t  mb-0 px-6 py-6">
                     <div class="text-center flex justify-between">
-                        <h6 class="text-blueGray-700 text-xl font-bold">
+                        <h6 class="text-xl font-bold">
                             Register Branch
                         </h6>
                         <!-- <button class="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150" type="button">
@@ -20,7 +21,7 @@
         </button> -->
                     </div>
                 </div>
-                <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+                <div class="flex-auto px-4 lg:px-10 py-5 pt-0">
                     <form>
                         <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
                             <!-- User Information -->
@@ -28,13 +29,11 @@
                         <div class="flex flex-wrap">
                             <div class="w-full lg:w-6/12 px-4">
                                 <div class="relative w-full mb-3">
-                                    <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                                    <label class="block uppercase text-xs font-bold mb-2"
                                         htmlfor="grid-password">
                                         Branch ID
                                     </label>
-                                    <input type="text"
-                                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                                        value="Branch-1">
+                                    <input class="border-0 px-3 py-3 rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" type="text" placeholder="Branch ID" class="input input-bordered" required />
                                 </div>
                             </div>
                             <div class="w-full lg:w-6/12 px-4">
@@ -46,35 +45,26 @@
               <input type="email" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="Delsan">
             </div> -->
 
-                            </div>
+                 </div> 
+                        
                             <div class="w-full lg:w-6/12 px-4">
                                 <div class="relative w-full mb-3">
                                     <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                                         htmlfor="grid-password">
                                         Branch Name
                                     </label>
-                                    <input type="text"
-                                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                                        value="Delsan">
+                                    <input class="border-0 px-3 py-3 rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" type="text" placeholder="Branch Name" class="input input-bordered" required />
+                                    <!-- <input type="text"
+                                        class="border-0 px-3 py-3 rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                                        > -->
                                 </div>
                             </div>
                             <div class="w-full lg:w-6/12 px-4">
-                                <!-- <div class="relative w-full mb-3">
-
-              <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                Address 2
-              </label>
-              <input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="Cebu">
-            </div> -->
-
+                               
                             </div>
                         </div>
 
-                        <hr class="mt-6 border-b-1 border-blueGray-300">
-
-                        <!-- <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-          Contact Information
-        </h6> -->
+                        
                         <div class="flex flex-wrap">
                             <div class="w-full lg:w-12/12 px-4">
                                 <div class="relative w-full mb-3">
@@ -83,8 +73,8 @@
                                         Address 1
                                     </label>
                                     <input type="text"
-                                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                                        value="Mandaue">
+                                        class="border-0 px-3 py-3 rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" type="text" placeholder="Address 1" class="input input-bordered" required
+                                        >
                                 </div>
                             </div>
                             <div class="w-full lg:w-12/12 px-4">
@@ -94,8 +84,8 @@
                                         Address 2
                                     </label>
                                     <input type="text"
-                                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                                        value="Cebu">
+                                        class="border-0 px-3 py-3 rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" type="text" placeholder="Address 2" class="input input-bordered" required
+                                        >
                                 </div>
                             </div>
                             <div class="w-full lg:w-4/12 px-4">
@@ -104,9 +94,9 @@
                                         htmlfor="grid-password">
                                         Date Opened
                                     </label>
-                                    <input type="email"
-                                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                                        value="12-20-2023">
+                                    <input type="text"
+                                        class="border-0 px-3 py-3 rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" type="text" placeholder="Date Opened" class="input input-bordered" required
+                                        >
                                 </div>
                             </div>
                             <div class="w-full lg:w-4/12 px-4">
@@ -116,8 +106,8 @@
                                         Type
                                     </label>
                                     <input type="text"
-                                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                                        value="1">
+                                        class="border-0 px-3 py-3 rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" type="text" placeholder="Type" class="input input-bordered" required
+                                        >
                                 </div>
                             </div>
                             <div class="w-full lg:w-4/12 px-4">
@@ -125,7 +115,7 @@
                             </div>
                         </div>
 
-                        <hr class="mt-6 border-b-1 border-blueGray-300">
+                        <!-- <hr class="mt-6 border-b-1 border-blueGray-300"> -->
 
                         <div class="flex flex-wrap">
                             <div class="w-full lg:w-12/12 px-4">
@@ -135,8 +125,8 @@
                                         Notes
                                     </label>
                                     <textarea type="text"
-                                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                                        rows="4"> No Notes</textarea>
+                                        class="border-0 px-3 py-3 rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" type="text" placeholder="Notes" class="input input-bordered" required
+                                        rows="4"></textarea>
                                 </div>
                             </div>
                             <div class="w-full lg:w-12/12 px-4">
@@ -145,16 +135,25 @@
                                         htmlfor="grid-password">
                                         Remark
                                     </label>
-                                    <textarea type="text"
-                                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                                        rows="4"> No Remarks</textarea>
+                                    <textarea type="text" 
+                                        class="border-0 px-3 py-3 rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" type="text" placeholder="Remark" class="input input-bordered" required
+                                        rows="4"></textarea>
                                 </div>
                             </div>
+                    <div class="w-full lg:w-12/12 px-4">        
+                    <div class="form-control mt-6">
+                        <button class="btn btn-primary">Register</button>
+                    </div>
+                    </div>
                         </div>
                     </form>
                 </div>
             </div>
 
         </div>
+
     </section>
+    </div>
+
+</div>
 @endsection
