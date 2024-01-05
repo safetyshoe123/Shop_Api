06d937f74c563db2be0f6b543dc1ec3220795e8b
OJT User 2 <ojt2@DBIC.local>
2024-01-05 14:39:11 +0800

    gates and roles are removed

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use App\Policies\AuthorizationPolicy;
-use App\Models\User;
 use Illuminate\Auth\Access\Response;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -27,25 +26,28 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // $this->registerPolicies();
-        //
-        Gate::define('isSuperAdmin', function (User $user) {
-            return $user->role == 'superAdmin';
-            // ? Response::allow()
-            // : Response::deny('You are not authorized');
-        });
+
+        // Gate::define('isSuperAdmin', function ($user) {
+        //     return $user->role == 'superAdmin';
+        // });
+        // Gate::define('isAdmin', function ($user) {
+        //     return $user->role == 'admin';
+        // });
+
+        // Gate::define('isManager', function ($user) {
+        //     return $user->role == 'manager';
+        // });
         // Gate::define('update-shop', [AuthorizationPolicy::class, 'update']);
         // Gate::define('getUser', [AuthorizationPolicy::class, 'getUser']);
 
-        Gate::define('isAdmin', function ($user) {
-            return $user->role == 'admin';
-        });
 
-        Gate::define('isManager', function ($user) {
-            return $user->role == 'manager';
-        });
-
-        // Gate::define('isEmployee', function ($user) {
-        //     return $user->status == 'employee';
+        //gate for creating employees in branches
+        // Gate::define('createEmployee', function ($user, $branch) {
+        //     return $user->restriction == $branch->branchId;
+        // });
+        //gate for viewing branches
+        // Gate::define('viewBranch', function ($user, $branch) {
+        //     return $user->restriction == $branch->branchId;
         // });
     }
 }
