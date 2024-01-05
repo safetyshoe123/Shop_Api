4875f343fef5d3f674b36a823cd05cc4244931b0
OJT User 2 <ojt2@DBIC.local>
2024-01-05 14:43:53 +0800

    nalimtan nga role sa AuthController na remove na

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -80,71 +80,71 @@ class AuthController extends Controller
         //     Gate::allows('isAdmin', $user) ||
         //     Gate::allows('isManager', $user)
         // ) {
-            $request->validate(
-                [
-                    'branchId' => 'required|string|max:10',
-                    'empId' => 'required|string|max:10',
-                    'lastName' => 'required|string|max:30',
-                    'firstName' => 'required|string|max:30',
-                    'middleName' => 'required|string|max:30',
-                    'password' => 'required|string|min:6',
-                    'status' => 'required|max:10',
-                    // 'role' => 'required',
-                    'dateHired' => 'required|date',
-                    'salary' => 'required',
-                    'notes' => 'required|string|max:255',
-                    'remark' => 'required|string|max:255',
-                ],
-                // [
-                //     'branchId.required' => 'Branch ID is required.',
+        $request->validate(
+            [
+                'branchId' => 'required|string|max:10',
+                'empId' => 'required|string|max:10',
+                'lastName' => 'required|string|max:30',
+                'firstName' => 'required|string|max:30',
+                'middleName' => 'required|string|max:30',
+                'password' => 'required|string|min:6',
+                'status' => 'required|max:10',
+                // 'role' => 'required',
+                'dateHired' => 'required|date',
+                'salary' => 'required',
+                'notes' => 'required|string|max:255',
+                'remark' => 'required|string|max:255',
+            ],
+            // [
+            //     'branchId.required' => 'Branch ID is required.',
 
-                //     'branchId.max' => 'Branch ID must not exceed 10 characters.',
-                //     'empId.required' => 'Employee ID is required.',
+            //     'branchId.max' => 'Branch ID must not exceed 10 characters.',
+            //     'empId.required' => 'Employee ID is required.',
 
-                //     'empId.max' => 'Employee ID must not exceed 10 characters.',
-                //     'lastName.required' => 'Last Name is required.',
-                //     'lastName.string' => 'Last Name must be a string.',
-                //     'lastName.max' => 'Last Name must not exceed 30 characters.',
-                //     'firstName.required' => 'First Name is required.',
-                //     'firstName.string' => 'First Name must be a string.',
-                //     'firstName.max' => 'First Name must not exceed 30 characters.',
-                //     'middleName.required' => 'Middle Name is required.',
-                //     'middleName.string' => 'Middle Name must be a string.',
-                //     'middleName.max' => 'Middle Name must not exceed 30 characters.',
-                //     'password.required' => 'Password is required.',
+            //     'empId.max' => 'Employee ID must not exceed 10 characters.',
+            //     'lastName.required' => 'Last Name is required.',
+            //     'lastName.string' => 'Last Name must be a string.',
+            //     'lastName.max' => 'Last Name must not exceed 30 characters.',
+            //     'firstName.required' => 'First Name is required.',
+            //     'firstName.string' => 'First Name must be a string.',
+            //     'firstName.max' => 'First Name must not exceed 30 characters.',
+            //     'middleName.required' => 'Middle Name is required.',
+            //     'middleName.string' => 'Middle Name must be a string.',
+            //     'middleName.max' => 'Middle Name must not exceed 30 characters.',
+            //     'password.required' => 'Password is required.',
 
-                //     'password.min' => 'Password must be at least 6 characters.',
-                //     'status.required' => 'Status is required.',
+            //     'password.min' => 'Password must be at least 6 characters.',
+            //     'status.required' => 'Status is required.',
 
-                //     'dateHired.required' => 'Date Hired is required.',
-                //     'dateHired.date' => 'Date Hired must be a valid date.',
-                //     'salary.required' => 'Salary is required.',
-                //     'notes.required' => 'Notes are required.',
-                //     'notes.string' => 'Notes must be a string.',
-                //     'notes.max' => 'Notes must not exceed 255 characters.',
-                //     'remark.required' => 'Remark is required.',
-                //     'remark.string' => 'Remark must be a string.',
-                //     'remark.max' => 'Remark must not exceed 255 characters.',
-                // ],
-            );
+            //     'dateHired.required' => 'Date Hired is required.',
+            //     'dateHired.date' => 'Date Hired must be a valid date.',
+            //     'salary.required' => 'Salary is required.',
+            //     'notes.required' => 'Notes are required.',
+            //     'notes.string' => 'Notes must be a string.',
+            //     'notes.max' => 'Notes must not exceed 255 characters.',
+            //     'remark.required' => 'Remark is required.',
+            //     'remark.string' => 'Remark must be a string.',
+            //     'remark.max' => 'Remark must not exceed 255 characters.',
+            // ],
+        );
 
-            $user = User::create([
-                'branchId' => $request->branchId,
-                'empId' => $request->empId,
-                'lastName' => $request->lastName,
-                'firstName' => $request->firstName,
-                'middleName' => $request->middleName,
-                'password' => Hash::make($request->password),
-                'status' => $request->status,
-                'role' => $request->role,
-                'restriction' => $request->restriction,
-                'dateHired' => $request->dateHired,
-                'salary' => $request->salary,
-                'notes' => $request->notes,
-                'remark' => $request->remark,
-            ]);
+        $user = User::create([
+            'branchId' => $request->branchId,
+            'empId' => $request->empId,
+            'lastName' => $request->lastName,
+            'firstName' => $request->firstName,
+            'middleName' => $request->middleName,
+            'password' => Hash::make($request->password),
+            'status' => $request->status,
+            // 'role' => $request->role,
+            'restriction' => $request->restriction,
+            'dateHired' => $request->dateHired,
+            'salary' => $request->salary,
+            'notes' => $request->notes,
+            'remark' => $request->remark,
+        ]);
 
-            return response()->json($user, 200);
+        return response()->json($user, 200);
         // } else {
         //     return response()->json(['message' => 'Access Denied! You are not authorized.'], 403);
         // }
