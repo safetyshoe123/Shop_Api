882612207f80e6bd6310ed46d03dcdd4817029fe
OJT User 2 <ojt2@DBIC.local>
2024-02-22 16:52:23 +0800

    feb 22

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -120,7 +120,7 @@ class AuthController extends Controller
     {
         return response()->json([
             'user' => Auth::user(),
-            'authorisation' => [
+            'authorization' => [
                 'token' => Auth::refresh(),
                 'type' => 'bearer',
             ]
